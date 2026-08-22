bde7a02a38fbfbce3f3d4ec3d68fa3907231b29e
somyacodes07 <ffsomyajeet@gmail.com>
2026-08-22 23:38:45 +0530

    Initial Commit

---
+++ MacCamBridge/MacCamBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCamBridgeApp.swift
+//  MacCamBridge
+//
+//  Created by Somyajeet on 22/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MacCamBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
